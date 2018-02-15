3e571a9ae2c819fb3601c8128748ba7d4975b260
neena38 <neena.mec@gmail.com>
2018-02-15 12:25:34 +0530

    1st shell commit

---
+++ shell/p1.sh
@@ -0,0 +1,9 @@
+x=$(logname)
+echo "Currently logged user and his logname : $x"
+echo "Current Shell:$SHELL"
+echo "Home directory:$HOME"
+x=$(arch)
+echo "OS Type : $x"
+echo "Path setting:$PATH"
+echo "Present Working Directory:$(pwd)"
+echo "Currently logged number of users: $(users)"
